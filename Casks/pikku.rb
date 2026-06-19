8adf1018863079e2884f38b796b4429cf30d98de
Pikku CI <ci@pikku.dev>
2026-06-19 07:35:40 +0000

    pikku 0.12.42

--- Casks/pikku.rb
+++ Casks/pikku.rb
@@ -1,14 +1,14 @@
 cask "pikku" do
-  version "0.12.41"
+  version "0.12.42"
 
   on_arm do
-    url "https://github.com/pikkujs/pikku/releases/download/%40pikku%2Fcli%400.12.41/pikku-darwin-arm64"
-    sha256 "910f44326e27505712ecad3e77b6e13b2383def33d8b7df9abcb515b7893fc6e"
+    url "https://github.com/pikkujs/pikku/releases/download/%40pikku%2Fcli%400.12.42/pikku-darwin-arm64"
+    sha256 "e342ca532426eca20b04256c713fe1de4654e41fd10aa15aa6cf9e7b8496c0f9"
     binary "pikku-darwin-arm64", target: "pikku"
   end
   on_intel do
-    url "https://github.com/pikkujs/pikku/releases/download/%40pikku%2Fcli%400.12.41/pikku-darwin-x64"
-    sha256 "7537d1a5fae330769176c73e3afdd3889baba5622238e31444c43c9eda24135f"
+    url "https://github.com/pikkujs/pikku/releases/download/%40pikku%2Fcli%400.12.42/pikku-darwin-x64"
+    sha256 "2862102ebefe36abd9d45e95d4874ceab7c71da4f784269b574ea02bf6fc4041"
     binary "pikku-darwin-x64", target: "pikku"
   end
 
